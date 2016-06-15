5235de67a881e64cde9523a7671b6d4a75037188
iskandar1023 <iiiiskandar14@gmail.com>
2016-06-15 17:23:30 +0700

    Velvet kernel assets
    Thanks zaclimon for base Quanta kernel

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #
-# Quanta Kernel build script
+# Velvet Kernel build script
 #
 # How to use:
 # Compiling wihtout any arguments will compile the AOSP version
@@ -9,14 +9,14 @@
 #
 #
 
-export CROSS_COMPILE="$HOME/uber-arm-eabi-5.3/bin/arm-eabi-"
-KERNEL_DIRECTORY="$HOME/Quanta-Mako"
-ANYKERNEL_DIRECTORY="$HOME/anykernel_msm"
+export CROSS_COMPILE="$HOME/android/tc/uber/out/arm-eabi-5.x-cortex-a15/bin/arm-eabi-"
+KERNEL_DIRECTORY="$HOME/android/kernel/Velvet-N4"
+ANYKERNEL_DIRECTORY="$HOME/android/kernel/anykernel_msm"
 JOBS=`grep -c "processor" /proc/cpuinfo`
 # Verify if the CM patches has already been applied. We don't want to apply them again if the compiling is stopped
 CM_CHECK=`grep -c "case MDP_YCBYCR_H2V1:" drivers/video/msm/mdp4_overlay.c`
 DEVICE="Mako"
-VERSION=17
+VERSION=3
 
 
 # Function responsible for download Anykernel if it's not found by the ANYKERNEL_DIRECTORY variable.
@@ -33,7 +33,7 @@ fi
 
 # Set the packaging information here. Specify if CM or AOSP.
 if [[ "$1" =~ "cm" || "$1" =~ "CM" ]] ; then
-kernelzip="Quanta-V$VERSION-CM-$DEVICE.zip"
+kernelzip="Velvet-V$VERSION-CM-$DEVICE.zip"
 
 # Apply the CM patches if the variant specified is CM.
 if [ $CM_CHECK -eq 0 ] ; then
@@ -41,7 +41,7 @@ git am CM/*
 fi
 
 else
-kernelzip="Quanta-V$VERSION-$DEVICE.zip"
+kernelzip="Velvet-V$VERSION-$DEVICE.zip"
 fi
 
 # Ensure that we're on the correct Anykernel branch
@@ -53,7 +53,7 @@ cd $KERNEL_DIRECTORY
 # Create a /zip directory if not present
 mkdir -p ./zip
 
-# Clean the /zip directory if there have been a zip file before (Mostly a version of Quanta compiled.)
+# Clean the /zip directory if there have been a zip file before (Mostly a version of Velvet compiled.)
 if [ -e zip/*.zip ] ; then
 rm -rf zip/*
 fi
@@ -62,7 +62,7 @@ fi
 let "VERSION -= 1"
 
 echo $VERSION > .version
-make quanta_defconfig
+make velvet_defconfig
 make -j$JOBS
 
 # Copy the contents of the Anykernel folder if we confirm that a zImage is present.
